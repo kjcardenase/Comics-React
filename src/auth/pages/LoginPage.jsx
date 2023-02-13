5f97a43c9cb407d7d1af7e56458271d3a2ed109a
Keyla Juleyma Cardenas Esteban <keyla.16f@gmail.com>
2023-02-13 15:44:44 -0500

    creando Logout

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,10 +1,18 @@
-import { Navigate, useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
 
 export const LoginPage = () => {
+    const { login } = useContext(AuthContext);
+
     const navigate = useNavigate();
 
     const onLogin = () => {
-        navigate('/', {
+        const lastPath = localStorage.getItem('lastPath') || '/';
+
+        login('Keyla Cárdenas');
+
+        navigate(lastPath, {
             replace: true,
         });
     };
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,9 +1,14 @@
+import { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth/context/AuthContext';
 
 export const Navbar = () => {
+    const { user, logout } = useContext(AuthContext);
+
     const navigate = useNavigate();
 
     const onLogout = () => {
+        logout();
         navigate('/login', {
             replace: true,
         });
@@ -12,7 +17,7 @@ export const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2 ">
             <Link className="navbar-brand" to="/">
-                Asociaciones
+                Comics
             </Link>
 
             <div className="navbar-collapse">
@@ -49,7 +54,7 @@ export const Navbar = () => {
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
                     <span className="nav-item nav-link text-info">
-                        Keyla Cárdenas
+                        {user?.name}
                     </span>
                     <button
                         className="nav-item nav-link btn"
